929fec90b0555f0e9480f66aba0a67a83c1f2c1c
Leonid Mashinskiy <leomash@gmail.com>
2023-12-08 11:11:07 +0300

    Day 8 solution code cleanup

--- py/8/8.py
+++ py/8/8.py
@@ -4,9 +4,7 @@ import re
 M = re.compile(r'(.*) = \((.*), (.*)\)')
 
 
-def solve1(infile):
-    ans = 0
-    cur = 'AAA'
+def parse(infile):
     tree = {}
     with open(infile) as f:
         cmd = f.readline().strip()
@@ -20,58 +18,46 @@ def solve1(infile):
             l = m.group(2)
             r = m.group(3)
             tree[fr] = (l, r)
-    print(tree)
-    while cur != 'ZZZ':
-        # print(cur)
+    return cmd, tree
+
+
+def num_steps(cmd, tree, start, check):
+    cur = start
+    ans = 0
+    while not check(cur):
         c = cmd[ans % len(cmd)]
         cur = tree[cur][0 if c == 'L' else 1]
         ans += 1
     return ans
 
 
-def lcm(a, b):
-    return (a * b) // math.gcd(a, b)
+def solve1(infile):
+    cmd, tree = parse(infile)
+    ans = num_steps(cmd, tree, 'AAA', lambda x: x == 'ZZZ')
+    return ans
 
 
 def solve2(infile):
-    ans = 0
+    cmd, tree = parse(infile)
     cur = []
-    tree = {}
-    with open(infile) as f:
-        cmd = f.readline().strip()
-        for line in f:
-            line = line.strip()
-            if not line:
-                continue
-            m = M.match(line)
-            assert m
-            fr = m.group(1)
-            l = m.group(2)
-            r = m.group(3)
-            if fr[2] == 'A':
-                cur.append(fr)
-            tree[fr] = (l, r)
+    for node in tree.keys():
+        if node[2] == 'A':
+            cur.append(node)
 
     steps = []
     for node in cur:
-        steps_node = 0
-        while node[2] != 'Z':
-            # print(cur)
-            c = cmd[steps_node % len(cmd)]
-            node = tree[node][0 if c == 'L' else 1]
-            steps_node += 1
+        steps_node = num_steps(cmd, tree, node, lambda x: x[2] == 'Z')
         steps.append(steps_node)
+
     print(steps)
-    ans = 1
-    for st in steps:
-        ans = lcm(st, ans)
+    ans = math.lcm(*steps)
     return ans
 
 
 def main():
-    # print(solve1('8_test.in'))
-    # print(solve1('8.in'))
-    # print(solve2('8_test2.in'))
+    print(solve1('8_test.in'))
+    print(solve1('8.in'))
+    print(solve2('8_test2.in'))
     print(solve2('8.in'))
 
 
